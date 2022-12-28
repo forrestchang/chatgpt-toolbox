9da4a5168875dd82a9930e99c2b9e513e998dcfd
Jiayuan <forrestchang7@gmail.com>
2022-12-29 01:59:47 +0800

    chore: Reinstall packages

--- mysite/settings.py
+++ mysite/settings.py
@@ -13,6 +13,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -125,3 +130,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+hello = os.getenv("HELLO")
+print(hello)
